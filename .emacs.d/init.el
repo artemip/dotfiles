0a482ed8b398eb73c8cb1e0d93b1cd2afe82c63c
Artem Pasyechnyk <artem.i.p@gmail.com>
2014-01-23 17:31:33 +0000

    Line numbers for emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,14 @@
+;; Set Linum-Mode on
+(global-linum-mode t)
+ 
+;; Linum-Mode and add space after the number
+(setq linum-format "%d ")
+
+(global-font-lock-mode t)
+
+;; Remove menu bar
+(menu-bar-mode 0)
+
+;; Get rid of annyoing ~ files
+(setq make-backup-files nil)
+
